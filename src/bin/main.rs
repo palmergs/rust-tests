27854400f1cc5685f83129e9c898b4a51bae6874
Galen Palmer <palmergs@gmail.com>
2020-04-20 20:16:20 -0400

    read from the entire resources directory

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,23 @@
 extern crate clap;
 use rust_names::NameBuilder;
 use std::fs;
+use std::path::Path;
+use std::io;
+
 use clap::{App, Arg};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-fn main() {
+fn main() -> io::Result<()> {
     let matches = App::new("randomlines")
         .version(VERSION)
         .about("Reads random lines from a file.!")
         .author("Galen P.")
-        .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .value_name("FILE")
-            .help("File to load the name strings")
+        .arg(Arg::with_name("dir")
+            .short("d")
+            .long("dir")
+            .value_name("DIR")
+            .help("Directory where resources are found")
             .takes_value(true))
         .arg(Arg::with_name("key")
             .short("k")
@@ -30,14 +33,28 @@ fn main() {
             .takes_value(true))
        .get_matches();
 
-    let file_name = matches.value_of("file")
-        .unwrap_or("names.txt");
-    let contents = fs::read_to_string(file_name)
-        .expect("unable to read name file");
+    let path = matches.value_of("dir").unwrap_or("resources");
+    let path = Path::new(path);
     let mut builder = NameBuilder::new();
-    match builder.parse(&contents) {
-        Ok(_) => (),
-        Err(msg) => println!("Unable to parse file '{}': err={}", file_name, msg),
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_file() {
+                match path.extension() {
+                    Some(ex) => {
+                        if ex == "txt" {
+                            let contents = fs::read_to_string(&path)?;
+                            match builder.parse(&contents) {
+                                Ok(_) => (),
+                                Err(msg) => println!("Unable to parse file{:?}: err={}", &path, msg),
+                            }
+                        }
+                    },
+                    None => ()
+                }
+            }
+        }
     }
 
     let key = matches.value_of("key").unwrap_or("");
@@ -48,6 +65,8 @@ fn main() {
         let count = count_str.parse::<i32>().unwrap();
         display_names(&builder, count, key);
     }
+
+    Ok(())
 }
 
 fn display_keys(builder: &NameBuilder) {
